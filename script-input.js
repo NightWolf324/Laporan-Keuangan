d18598fd1c55e9eeabcccef8822f649c69207afe
NightWolf <88887015+NightWolf324@users.noreply.github.com>
2023-08-27 23:27:49 +0700

    Update some feature

--- script-input.js
+++ script-input.js
@@ -7,12 +7,45 @@ inputForm.addEventListener('submit', function(event) {
     const keterangan = document.getElementById('keterangan').value;
     const nominal = parseFloat(document.getElementById('nominal').value);
 
-    // Simpan data ke localStorage
-    const newData = { tanggal, keterangan, nominal };
-    const existingData = JSON.parse(localStorage.getItem('keuangan')) || [];
-    existingData.push(newData);
-    localStorage.setItem('keuangan', JSON.stringify(existingData));
+    // Ambil data dari cookies (jika ada)
+    // const existingData = getExistingDataFromCookies();
+
+    // Ambil data dari LocalStorage (jika ada)
+    const existingData = getExistingDataFromLocalStorage();
+
+    // Tambahkan data baru ke array existingData
+    existingData.push({ tanggal, keterangan, nominal });
+
+    // Simpan data ke cookies
+    // saveDataToCookies(existingData);
+
+    // Simpan data ke LocalStorage
+    saveDataToLocalStorage(existingData);
 
     // Setelah data ditambahkan, bisa mengarahkan kembali ke halaman pertama
     window.location.href = 'index.html';
 });
+
+// Fungsi untuk mengambil data dari cookies
+// function getExistingDataFromCookies() {
+//     const cookieData = decodeURIComponent(document.cookie.replace(/(?:(?:^|.*;\s*)keuanganData\s*\=\s*([^;]*).*$)|^.*$/, "$1"));
+//     return cookieData ? JSON.parse(cookieData) : [];
+// }
+
+// Fungsi untuk mengambil data dari LocalStorage
+function getExistingDataFromLocalStorage() {
+    const localStorageData = localStorage.getItem('keuangan');
+    return localStorageData ? JSON.parse(localStorageData) : [];
+}
+
+// Fungsi untuk menyimpan data ke cookies
+// function saveDataToCookies(data) {
+//     const dataString = JSON.stringify(data);
+//     document.cookie = `keuanganData=${encodeURIComponent(dataString)}; expires=Fri, 31 Dec 9999 23:59:59 GMT; path=/`;
+// }
+
+// Fungsi untuk menyimpan data ke LocalStorage
+function saveDataToLocalStorage(data) {
+    localStorage.setItem('keuangan', JSON.stringify(data));
+}
+
--- script-tabel.js
+++ script-tabel.js
@@ -5,24 +5,95 @@ function formatRupiah(angka) {
     return new Intl.NumberFormat('id-ID', { style: 'currency', currency: 'IDR' }).format(angka);
 }
 
-// Ambil data dari localStorage dan tampilkan pada tabel
-const storedData = JSON.parse(localStorage.getItem('keuangan')) || [];
-
-// Fungsi untuk menghapus data berdasarkan indeks
-function hapusData(index) {
-    const storedData = JSON.parse(localStorage.getItem('keuangan')) || [];
-    storedData.splice(index, 1);
-    localStorage.setItem('keuangan', JSON.stringify(storedData));
-    window.location.reload(); // Muat ulang halaman untuk memperbarui tampilan
+// Fungsi untuk menghitung total nominal
+function hitungTotalNominal(data) {
+    let total = 0;
+    data.forEach(item => {
+        total += item.nominal;
+    });
+    return total;
 }
 
-storedData.forEach((data, index) => {
-    const newRow = document.createElement('tr');
-    newRow.innerHTML = `
-        <td>${data.tanggal}</td>
-        <td>${data.keterangan}</td>
-        <td>${formatRupiah(data.nominal)}</td>
-        <td><button class="btn btn-danger" onclick="hapusData(${index})">Hapus</button></td>
+// Fungsi untuk menghapus data berdasarkan indeks
+// function hapusData(index) {
+//     const storedData = getExistingDataFromCookies();
+//     storedData.splice(index, 1);
+//     saveDataToCookies(storedData);
+//     updateTableAndTotal(storedData); // Perbarui tampilan tabel dan total
+// }
+
+function hapusData(index) {
+    const storedData = getExistingDataFromLocalStorage();
+    storedData.splice(index, 1);
+    saveDataToLocalStorage(storedData);
+    updateTableAndTotal(storedData); // Perbarui tampilan tabel dan total
+}
+
+// Fungsi untuk memperbarui tampilan tabel dan total
+function updateTableAndTotal(data) {
+    tableBody.innerHTML = ''; // Kosongkan tabel sebelum diperbarui
+
+    data.forEach((data, index) => {
+        const newRow = document.createElement('tr');
+        newRow.innerHTML = `
+            <td>${data.tanggal}</td>
+            <td>${data.keterangan}</td>
+            <td>${formatRupiah(data.nominal)}</td>
+            <td><button class="btn btn-danger" onclick="hapusData(${index})">Hapus</button></td>
+        `;
+        tableBody.appendChild(newRow);
+    });
+
+    // Tampilkan total nominal di bawah tabel
+    const totalRow = document.createElement('tr');
+    totalRow.innerHTML = `
+        <td colspan="2" class="text-right"><strong>Total:</strong></td>
+        <td><strong>${formatRupiah(hitungTotalNominal(data))}</strong></td>
     `;
-    tableBody.appendChild(newRow);
-});
+    tableBody.appendChild(totalRow);
+}
+
+// Fungsi untuk mengambil data dari cookies
+// function getExistingDataFromCookies() {
+//     const cookieData = decodeURIComponent(document.cookie.replace(/(?:(?:^|.*;\s*)keuanganData\s*\=\s*([^;]*).*$)|^.*$/, "$1"));
+//     return cookieData ? JSON.parse(cookieData) : [];
+// }
+
+// Fungsi untuk mengambil data dari LocalStorage
+function getExistingDataFromLocalStorage() {
+    const localStorageData = localStorage.getItem('keuangan');
+    return localStorageData ? JSON.parse(localStorageData) : [];
+}
+
+// Inisialisasi halaman dengan data dari cookies
+// const storedData = getExistingDataFromCookies();
+
+// Inisialisasi halaman dengan data dari LocalStorage
+const storedData = getExistingDataFromLocalStorage();
+updateTableAndTotal(storedData);
+
+// Fungsi untuk mengunduh data keuangan dalam bentuk PDF
+function unduhPDF() {
+    const doc = new jsPDF();
+    
+    doc.text("Laporan Data Keuangan", 10, 10);
+    
+    const tableData = [];
+    const tableHeaders = ["Tanggal", "Keterangan", "Nominal"];
+    
+    storedData.forEach(data => {
+        tableData.push([data.tanggal, data.keterangan, formatRupiah(data.nominal)]);
+    });
+    
+    doc.autoTable({
+        head: [tableHeaders],
+        body: tableData,
+        startY: 20
+    });
+    
+    doc.save("Laporan_Data_Keuangan.pdf");
+}
+
+// Tambahkan event listener pada tombol unduh PDF
+const downloadPDFBtn = document.getElementById('download-pdf-btn');
+downloadPDFBtn.addEventListener('click', unduhPDF);
